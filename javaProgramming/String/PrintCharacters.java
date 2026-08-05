f6bb94f896b874f566b4d769bae014515b2d0913
Mahima <mahimamundra.12@gmail.com>
2026-08-05 11:17:32 +0530

    few string problems

---
+++ javaProgramming/String/PrintCharacters.java
@@ -0,0 +1,16 @@
+package String;
+
+public class PrintCharacters {
+
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        char [] letter = str.toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+        for(int i = 0; i < letter.length; i++) { 
+            System.out.println(letter[i]);
+        }
+    }
+    
+}
