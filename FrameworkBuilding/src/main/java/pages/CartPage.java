6646003fe91fa2a50b36c4027c6f4580a42fa74b
mahimamundra12-stack <mahimamundra.12@gmail.com>
2026-08-24 16:27:03 +0530

    Move CartPage into FrameworkBuilding

---
+++ FrameworkBuilding/src/main/java/pages/CartPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+
+public class CartPage {
+    private final By backpack = By.id("item_4_title_link");
+
+    public boolean isBackpackDisplayed() {
+        return DriverFactory.getDriver().findElement(backpack).isDisplayed();
+    }
+}
