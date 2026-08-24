3ed3b5434ab5e9c230fb8daff0157a6e19e30166
mahimamundra12-stack <mahimamundra.12@gmail.com>
2026-08-24 16:26:34 +0530

    Move DriverFactory into FrameworkBuilding

---
+++ FrameworkBuilding/src/main/java/driver/DriverFactory.java
@@ -0,0 +1,38 @@
+package driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public final class DriverFactory {
+    private static final ThreadLocal<WebDriver> DRIVER = new ThreadLocal<>();
+
+    private DriverFactory() {}
+
+    public static void initDriver(String browser) {
+        WebDriver driver;
+        switch (browser.toLowerCase()) {
+            case "chrome" -> driver = new ChromeDriver();
+            case "firefox" -> driver = new FirefoxDriver();
+            default -> throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+        driver.manage().window().maximize();
+        DRIVER.set(driver);
+    }
+
+    public static WebDriver getDriver() {
+        WebDriver driver = DRIVER.get();
+        if (driver == null) {
+            throw new IllegalStateException("WebDriver is not initialized");
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        WebDriver driver = DRIVER.get();
+        if (driver != null) {
+            driver.quit();
+            DRIVER.remove();
+        }
+    }
+}
