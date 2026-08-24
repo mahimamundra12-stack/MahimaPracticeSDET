6b7f95f301ee3878d6547798c16ac23730f9237d
mahimamundra12-stack <mahimamundra.12@gmail.com>
2026-08-24 16:27:08 +0530

    Move BaseTest into FrameworkBuilding

---
+++ FrameworkBuilding/src/test/java/base/BaseTest.java
@@ -0,0 +1,23 @@
+package base;
+
+import config.ConfigReader;
+import driver.DriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        DriverFactory.initDriver(ConfigReader.get("browser"));
+        driver = DriverFactory.getDriver();
+        driver.get(ConfigReader.get("baseUrl"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverFactory.quitDriver();
+    }
+}
